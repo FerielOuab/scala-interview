2caf71722f4c47fc429a03535de90fe495c132d8
Dinesh <g.dinesh95140@gmail.com>
2020-06-08 11:59:52 +0200

    whatwrong1

--- app/BasicScala.scala
+++ app/BasicScala.scala
@@ -4,40 +4,41 @@ package com.particeep.test
   * This is basic language questions so don't use external library or build in function
   */
 object BasicScala {
+  println("Power: "+ power(2, 3));
+  println("Email: "+ isEmail("particeep@gmail.com"));
+  println("Email: "+ isEmail("feriel_ouabdesselam@gmail.com"));
+  var m1 = Map("sort_by" -> "name", "order_by" -> "asc", "user_id" -> "12");
+  println("URL 1 :"+encodeParamsInUrl(m1));
+  println("URL 2 :"+encodeParamsInUrl(Map()));
 
 
-  /**
-    * Encode parameter in url format
-    *
-    * Example:
-    *
-    * input  : Map("sort_by" -> "name", "order_by" -> "asc", "user_id" -> "12")
-    * output : "?sort_by=name&order_by=asc&user_id=12"
-    *
-    * input  : Map()
-    * output : ""
-    */
-  def encodeParamsInUrl(params: Map[String, String]): String = ???
+  def encodeParamsInUrl(params: Map[String, String]): String = {
+    if(params.isEmpty) {
+      return "";
+    }
+    var string:String = "?";
+    for ((k,v) <- params)
+    {
+      var s1:String = k+"="+v+"&";
+      string = string.concat(s1);
+    }
+    return string.substring(0, string.length-1);
+  }
 
 
-  /**
-    * Test if a String is an email
-    */
   def isEmail(maybeEmail: String): Boolean = ???
+  """([\w\.\-\_]+)@(\w+\.\w+)""".r.unapplySeq(email).isDefined
 
-
-  /**
-    * Compute i ^ n
-    *
-    * Example:
-    *
-    * input : (i = 2, n = 3) we compute 2^3 = 2x2x2
-    * output : 8
-    *
-    * input : (i = 99, n = 38997)
-    * output : 1723793299
-    */
-  def power(i:Int, n:Int):Int = ???
-
+  def power(i:Int, n:Int):Int =
+  {
+    @tailrec def powerAcc(acc: Int, n: Int): Int =
+    {
+      if (n == 0)
+        acc
+      else
+        powerAcc(i * acc, n - 1)
+    }
+    powerAcc(1, n)
+  }
 
 }
--- app/MapYourMap.scala
+++ app/MapYourMap.scala
@@ -10,5 +10,25 @@ object MapYourMap {
   val devNames = Map("dev1" -> "Pierre", "dev2" -> "Remy", "dev3" -> "Noe", "dev4" -> "Alexandre")
   val devDepartments = Map("dev1" -> "analytics", "dev2" -> "frontend", "dev3" -> "api", "dev4" -> "frontend")
 
-  val namesInDepartments:Map[String, List[String]] = ???
+  var map1: mutable.Map[String, List[String]] = scala.collection.mutable.Map[String, List[String]]();
+  for((k, v) <- devDepartments)
+  {
+    for ((k1, v1) <- devNames)
+    {
+      if(k==k1)
+      {
+        if(!map1.contains(v))
+        {
+          map1 = map1 +(v -> List(v1));
+        }
+        else
+        {
+          map1 = map1 + (v ->(v1::map1.get(v).toList(0)));
+        }
+      }
+    }
+
+  }
+  val namesInDepartments: mutable.Map[String, List[String]] = map1;
+  println(namesInDepartments.toMap);
 }
